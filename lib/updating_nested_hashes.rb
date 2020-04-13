193d5b16d18cf27528100dde8c53b931efc49d94
Dorothy John <dorothyy.john@gmail.com>
2020-04-13 15:25:21 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -62,7 +62,7 @@ def add_characters
    
 #Add a key on the same level nesting as montague & capulet
 epic_tragedy[:additional_characters] = [
-  {name: ""},
+  {name: "Prince Escalus"},
   {name: ""}
   ]
 
